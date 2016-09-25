a911af39b01bea3aca2ff3a0b6f9365c4b58d836
Fetzie <tom_anson@t-online.de>
2016-09-25 17:34:31 +0200

    Update createDB.sql

--- dbCreator/createDB.sql
+++ dbCreator/createDB.sql
@@ -7,74 +7,77 @@ USING database 'phpClass';
 CREATE table customers
 {
 id int not null AUTO_INCREMENT,
-firstName VARCHAR(55) not null,
-lastName VARCHAR (55) not null,
-emailAddress VARCHAR (55) not null,
+firstname VARCHAR(55) not null,
+lastname VARCHAR (55) not null,
+emailaddress VARCHAR (55) not null,
 address1 VARCHAR (55) not null,
 houseNumber VARCHAR (55) not null,
 zipcode VARCHAR (55) not null,
 city VARCHAR (55) not null,
 PRIMARY KEY (id)
-}
+};
 
 CREATE table customerpaymentmethods
 {
 id int(20) not null AUTO_INCREMENT,
 customerid int(20) not null ,
-paymentMethod int(20) not null,
-cardNumber VARCHAR(60) not null,
-cardName VARCHAR(60) not null,
+paymentmethod int(20) not null,
+cardnumber VARCHAR(60) not null,
+cardname VARCHAR(60) not null,
 expires VARCHAR(60) not null,
-secNumber VARCHAR(60),
-billingAddress VARCHAR(500) not null,
+secnumber VARCHAR(60),
+billingaddress VARCHAR(500) not null,
 PRIMARY KEY (id)
 FOREIGN KEY customerid REFERENCES customers(id) ON UPDATE CASCADE ON DELETE RESTRICT
 FOREIGN KEY paymentmethod REFERENCES paymentOptions(id) ON UPDATE CASCADE ON DELETE RESTRICT 
-}
+};
 
 CREATE table paymentOptions{
 id int,
-paymentType VARCHAR(10),
+paymenttype VARCHAR(10),
 PRIMARY KEY (id)
-}
+};
 
 CREATE table advertisement{
-
 id int,
-text VARCHAR (5000),
-picturePath VARCHAR (240),
-customerid int ,
-createDate int,
-expireDate int,
-duration int,
-transactionid int,
-categoryid int
+text VARCHAR(5000),
+picturepath VARCHAR(240),
+customerid int not null,
+createdate int not null,
+expiredate int not null,
+duration int not null,
+transactionid int not null,
+categoryid int not null,
 PRIMARY KEY (id)
 FOREIGN KEY (customerid) REFERENCES customers(id) ON UPDATE CASCADE ON DELETE RESTRICT
 FOREIGN KEY (transactionid) REFERENCES transactions(id) ON UPDATE CASCADE ON DELETE RESTRICT
-}
+};
 
 CREATE table advertisement2category{
-advertisementid int,
-categoryid int,
+advertisementid int not null,
+categoryid int not null,
 FOREIGN KEY (categoryid) REFERENCES category(id) ON UPDATE CASCADE ON DELETE RESTRICT
 FOREIGN KEY (advertisementid) REFERENCES advertisement(id) ON UPDATE CASCADE ON DELETE RESTRICT
-
-}
+};
 
 CREATE table category{
 id int not null auto_increment(0),
-name VARCHAR(255),
-parentid int,
+name VARCHAR(255) not null,
+parentid int not null,
 PRIMARY KEY (id)
-}
+};
 
 CREATE table transactions{
-id int AUTO_INCREMENT,
-customerid int,
-price double,
-paymentoptionid VARCHAR,
+id int not null AUTO_INCREMENT,
+customerid int not null,
+price double not null,
+paymentoptionid int not null,
+advertisementid int not null,
+paymentdate int not null,
+paymentmethodid int not null,
 PRIMARY KEY (id)
 FOREIGN KEY (customerid) REFERENCES customer(id) ON UPDATE CASCADE ON DELETE RESTRICT
 FOREIGN KEY (paymentoption) REFERENCES paymentoption(id) ON UPDATE CASCADE ON DELETE RESTRICT
-]
+FOREIGN KEY (advertisementid) REFERENCES advertisement(id) ON UPDATE CASCADE ON DELETE RESTRICT
+FOREIGN KEY (paymentmethodid) REFERENCES customerpaymentmethods(id) ON UPDATE CASCADE ON DELETE RESTRICT
+};
