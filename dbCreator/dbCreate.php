de0bf03b2d4316ea32458be91899f9543bc9cc07
Tom <tom_anson@t-online.de>
2016-09-25 17:13:16 +0200

    changes to createDB sql

--- dbCreator/dbCreate.php
+++ dbCreator/dbCreate.php
@@ -1,12 +1,33 @@
 <?php 
+/**
+ * executes contents of createDB.sql
+ * 
+ * @param unknown $fileName
+ * @param unknown $host
+ * @param unknown $user
+ * @param unknown $password
+ */
+function writeDB($fileName, $host, $user, $password){
 
-function writeDB($DBLink){
+	$dbLink = mysqli_connect($host,$user,$password);
+	
+	if (!$dbLink){
+	
+		echo date("Y-M-d G:i:s", time()) . " : [dbHandler.DBLogin] Connection to " . $database . " failed" . "</br>";
+		echo date("Y-M-d G:i:s", time()) . " : [dbHandler.DBLogin] Debugging Err.No: " . mysqli_errno($dbLink) . "</br>" . PHP_EOL;
+		echo date("Y-M-d G:i:s", time()) . " : [dbHandler.DBLogin] Debugging Error: " . mysqli_error($dbLink) . "</br>" . PHP_EOL;
+		exit;
+	}
+	echo date("Y-M-d G:i:s", time()) . " : [dbHandler.DBLogin] dbconnection to " . $database . " was successful" . "</br>" . PHP_EOL;
+	echo date("Y-M-d G:i:s", time()) . " : [dbHandler.DBLogin] host information: " . mysqli_get_host_info($dbLink) . "</br>" . PHP_EOL;
+	return $dbLink;
+	
+	$sql = explode(";",file_get_contents($fileName));
 
-$sql = explode(";",file_get_contents("createDB.sql"));
-
-foreach ($sql as $query)
-	mysqli_query($DBLink, $query);
+	foreach ($sql as $query)
+		mysqli_query($DBLink, $query);
 }
 
+writeDB("createDB.sql", "127.0.0.1", "root", "");
 
 ?>
