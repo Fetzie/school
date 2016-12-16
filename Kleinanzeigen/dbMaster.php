6189f2996385be2c2559c120ede80d5316416f83
Tom <tom_anson@t-online.de>
2016-12-16 18:18:32 +0100

    user login and creation works known issues: login is executed every time the page is refreshed -> error message

--- Kleinanzeigen/dbMaster.php
+++ Kleinanzeigen/dbMaster.php
@@ -3,9 +3,10 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "phpclass2";
+$port = "3306";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if (!$conn)
